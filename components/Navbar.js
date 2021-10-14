6fe1fcaa2847c99a1be0162760e7ba2c1dd621c6
shearertzach <shearertzach@gmail.com>
2021-10-14 12:39:41 -0500

    Navbar style changes

--- components/Navbar.js
+++ components/Navbar.js
@@ -3,7 +3,7 @@ import Sidebar from './Sidebar'
 
 export default function Navbar() {
   return (
-    <div className='p-4 py-6 border-b-2 border-gray-400 mb-6 flex justify-between items-center'>
+    <div className='px-8 py-5 border-b-2 border-gray-400 mb-6 flex justify-between items-center'>
       <p className='text-gray-500 text-3xl font-semibold'>Paper Scanner</p>
       <Sidebar />
     </div>
--- components/Sidebar.js
+++ components/Sidebar.js
@@ -6,7 +6,7 @@ export default function Sidebar() {
 
   return (
     <div>
-      <div className='cursor-pointer' onClick={() => setSidebar(!sidebar)}>
+      <div className='cursor-pointer p-1 hover:bg-gray-200 rounded-md transition-all' onClick={() => setSidebar(!sidebar)}>
         <svg xmlns="http://www.w3.org/2000/svg" className="h-8 w-8" fill="none" viewBox="0 0 24 24" stroke="currentColor">
           <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16M4 18h16" />
         </svg>
